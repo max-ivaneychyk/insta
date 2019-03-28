24c4b33aa9df3f275558b1c2958df6c172175c08
max-ivaneychyk <maxym2010i@gmail.com>
2019-03-28 18:03:15 +0200

    Add files via upload

---
+++ precache-manifest.89777f0fd5854e0c0511e2286f7d7fea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4ad8ad0421109e3c87f",
+    "url": "/static/js/main.ba837a74.chunk.js"
+  },
+  {
+    "revision": "4bd1d4eea210ddd5bfad",
+    "url": "/static/js/2.ea93b2ed.chunk.js"
+  },
+  {
+    "revision": "d4ad8ad0421109e3c87f",
+    "url": "/static/css/main.e7c9158e.chunk.css"
+  },
+  {
+    "revision": "9e729a22c1c6b6130bcfeb6de479b12a",
+    "url": "/index.html"
+  }
+];
